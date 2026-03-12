1e4a74f4c2a743742ac7dd73a3fc72cc32b3ac4c
Đặng Trần Quốc Trinh <dangtrinh768@gmail.com>
2026-03-13 00:31:59 +0700

    BT3

--- bt5/src/main/java/buoi5/bt5/repository/BookRepository.java
+++
@@ -1,5 +0,0 @@
-package buoi5.bt5.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import buoi5.bt5.model.Book;
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
